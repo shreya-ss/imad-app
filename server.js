7fc9edffbbf45f709500c5ac0a45297eaae7375e
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-15 20:12:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,50 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+  title: 'Article One | Shreya Shambhawi Singh',  
+  heading: 'Article One',
+  date: '25 March,2017',
+  content:`<p>
+             This is the content of my first article
+           </p>`
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var httpTemplate=`
+    <head>
+        <title>
+            ${title};
+        </title>
+        <meta name="viewport" content="width-device-width, initial-scale-1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+                </p>
+            </div>
+        </div>
+    </body>
+    </html>`
+    return httpTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -26,7 +70,7 @@ app.get('/article-three',function(req,res){
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
