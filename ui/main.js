ea09787d7dc5878e836ec2893a56b48a936b7561
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-16 22:32:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text")
 element.innerHTML = "New Value :P"
+
+//Move image
+
+var img = document.getElementById("madi")
+img.onclick=function(){
+    img.style.marginLeft="100px"
+}
