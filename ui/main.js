5a8b618a0392d9b7456257f15d5dbe1a75260e9b
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-16 22:44:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,10 +19,10 @@ var img = document.getElementById("madi")
 var marginLeft=0;
 
 function moveRight(){
-    marginLeft=marginLeft+1;
+    marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+'px';
 }
 
 img.onclick=function(){
-    var interval=setInterval(moveRight,5)
+    var interval=setInterval(moveRight,50)
 }
