1582ef9edf8fc07c6d494d14f0189598e86428d3
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-16 22:40:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,22 @@ element.innerHTML = "New Value :P"
 
 //Move image
 
-var img = document.getElementById("madi")
+/*var img = document.getElementById("madi")
 img.onclick=function(){
     img.style.marginLeft="100px"
+}*/
+
+//Gradually move image
+
+var img = document.getElementById("madi")
+
+var marginLeft=0;
+
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
+img.onclick=function(){
+    var interval=setInterval(moveRight,100)
 }
