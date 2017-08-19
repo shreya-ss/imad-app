c94e06ac122f8fef39437444795344f0d2343105
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-19 13:26:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,11 +71,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter',function(req,res) {
-   counter=counter+1;
-   res.send(counter.toString());
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -85,6 +81,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res) {
+   counter=counter+1;
+   res.send(counter.toString());
+});
 /*app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
