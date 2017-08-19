909cc61bd670968f3d40622cd95129c6c367df98
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-19 13:22:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/:articleName',function(req,res){
 
 app.get('/ui/main.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
-})
+});
 
 var counter = 0;
 app.get('/counter',function(req,res){
