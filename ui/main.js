008b8d134b942ae6cec90eb16fc75f298504467e
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-19 17:24:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,6 +38,7 @@ submit.onclick= function(){
     for(var i=0 ; i<names.length ; i++){
         list == '<li>' + names[i] + '</li>';
     }
+    Console.log(list);
     var ul=document.getElementById('namelist');
     ul.innerHTML = list;
 };
