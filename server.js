21a0608e0676a267834dfac7113a9a155b25a3c5
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-31 07:58:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ function hash(input,salt){
 
 app.get('/hash/:input',function(req,res){
    var hashedString = hash(req.params.input,'this-is-some-random-string');
-   res.send(hashedSting);
+   res.send(hashedString);
 });
 
 var names=[];
