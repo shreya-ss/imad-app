134c4b1e421f3fdcc2869c8acb4402ef0c306b5c
shreya-ss <30780505+shreya-ss@users.noreply.github.com>
2017-08-31 07:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/counter',function(req,res) {
 });
 
 function hash(input,salt){
-    var hashed = crypto.pbkdf2(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
 
